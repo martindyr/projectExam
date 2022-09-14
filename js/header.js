d1c48e8a7750eb915bc78e7dca8c4783732094d4
martindyr <md@triark.no>
2022-09-14 14:04:04 +0200

    Nav upgrade

--- js/header.js
+++ js/header.js
@@ -11,9 +11,9 @@ header.innerHTML = `
     <!-- Logo -->
     </div>
     <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
+        <li><a href="index.html">Home</a></li>
+        <li><a href="about.html">About</a></li>
+        <li><a href="contact.html">Contact</a></li>
     </ul>
 </nav>
 `
